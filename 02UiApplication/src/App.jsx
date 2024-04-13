4e028b0a994632c67b44acb81a5f053e78ed4f08
1anshu-stack <71838931+1anshu-stack@users.noreply.github.com>
2024-04-13 16:39:39 +0000

    gg

--- 02UiApplication/src/App.jsx
+++ 02UiApplication/src/App.jsx
@@ -1,11 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/Home";
+
+import './style/App.scss'
+import './style/header.scss'
+import './style/home.scss'
 
 function App() {
   return (
     <>
       <Router>
+        <Header />
         <Routes>
-          <Route />
+          <Route path="/" element={<Home />} />
         </Routes>
       </Router>
     </>

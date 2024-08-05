5557195fbf7614c912ab4a231c047b02e4f68d79
1anshu-stack <71838931+1anshu-stack@users.noreply.github.com>
2024-08-05 19:06:25 +0000

    navcmp

---
+++ 07BrandPage/src/components/ButtonSign.jsx
@@ -0,0 +1,9 @@
+function ButtonSign({btnName}){
+    return (
+        <>
+            <div className="bg-red-700 py-1 px-6 rounded hover:bg-red-600 pointer cursor-pointer text-white font-medium">{btnName}</div>
+        </>
+    )
+}
+
+export default ButtonSign;
---
+++ 07BrandPage/src/components/Nav.jsx
@@ -0,0 +1,25 @@
+import ButtonSign from "./ButtonSign";
+
+function Nav(){
+    
+    return (
+        <>
+          <div className="w-full h-20 px-16 py-4 flex justify-between shadow-md">
+            <div>
+                <img src="https://github.com/anshuopinion/React-10-Projects/blob/project-1/project-1/public/images/brand_logo.png?raw=true" alt="" srcset="" />
+            </div>
+            <div className="flex gap-5 text-lg font-bold mt-2">
+                <div className="cursor-pointer underline underline-offset-8">Menu</div>
+                <div className="cursor-pointer" >Location</div>
+                <div className="cursor-pointer">About</div>
+                <div className="cursor-pointer">contact</div>
+            </div>
+            <div className="mt-2">
+                <ButtonSign btnName="Login" />
+            </div>
+          </div>
+        </>
+    )
+}
+
+export default Nav;
